bbd559016259af44b95e6ec85ddb345106541537
Luca Nerlich <luca.nerlich@googlemail.com>
2021-12-05 18:08:40 +0100

    update ListEntry.kt text displayed

--- src/main/kotlin/composables/ListEntry.kt
+++ src/main/kotlin/composables/ListEntry.kt
@@ -4,6 +4,8 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import entities.Quote
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -11,10 +13,22 @@ import entities.Quote
 fun ListEntry(quote: Quote) {
     ListItem(
         text = {
-            Text("Price: " + quote.globalQuote.price + "$")
+            Text(
+                text = "Price: " + quote.globalQuote.price + "$"
+            )
         },
         secondaryText = {
-            Text("Change: " + quote.globalQuote.change)
+            Text(
+                text = "Change: " + quote.globalQuote.change,
+                fontWeight = FontWeight.Bold,
+                color = if (quote.globalQuote.change > 0) Color.Green else Color.Red
+            )
+        },
+        overlineText = {
+            Text(
+                text = "Previous Close: " + quote.globalQuote.previousClose,
+                fontWeight = FontWeight.Light,
+            )
         },
         icon = {
             Text(quote.globalQuote.symbol)
